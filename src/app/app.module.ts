ea30b1127b9adecacf2f5c9dc557cbae3e3b73ff
PraveenK <kprave333@gmail.com>
2023-03-24 13:01:10 +0530

    commit my angular code in git

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// used to create fake backend
+import { fakeBackendProvider } from './_helpers';
+
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { appRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home';
+import { LoginComponent } from './login';
+import { RegisterComponent } from './register/register.component';
+import { AlertComponent } from './_components/alert.components';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        appRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+        AlertComponent
+    ],
+    
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+
+        // provider used to create fake backend
+        fakeBackendProvider
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { };
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { first } from 'rxjs/operators';
+
+import { User } from '../_models';
+import { UserService, AuthenticationService } from '../_services';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit, OnDestroy {
+    currentUser: User = new User;
+    users: User[] = [];
+  currentUserSubscription: Subscription;
+
+  constructor(
+      private authenticationService: AuthenticationService,
+      private userService: UserService
+  ) {
+      this.currentUserSubscription = this.authenticationService.currentUser.subscribe((user: User) => {
+          this.currentUser = user;
+      });
+  }
+
+  ngOnInit() {
+      this.loadAllUsers();
+  }
+
+  ngOnDestroy() {
+      // unsubscribe to ensure no memory leaks
+      this.currentUserSubscription.unsubscribe();
+  }
+
+  deleteUser(id: number) {
+      this.userService.delete(id).pipe(first()).subscribe(() => {
+          this.loadAllUsers()
+      });
+  }
+
+  private loadAllUsers() {
+      this.userService.getAll().pipe(first()).subscribe(users => {
+          this.users = users;
+      });
+  }
+}
